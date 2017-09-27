1f8cefb5e6adb943e04dc942a790718c7f0027e6
Hugo Roque <hugolnx@gmail.com>
2017-09-27 07:33:48 -0300

    Replacing tab with spaces on luascripts

--- lib/lua/renew_lock.lua
+++ lib/lua/renew_lock.lua
@@ -1,14 +1,14 @@
 local tolerance_start_time = redis.call("GET", KEYS["user_tolerance_start"])
 if tolerance_start_time
-	and tonumber(tolerance_start_time) < tonumber(ARGV["tolerance_time_limit"]) then
-	redis.call("ZREMRANGEBYRANK", KEYS["user_lock"], 0, tonumber(ARGV["max_locks"])*-1-1)
+  and tonumber(tolerance_start_time) < tonumber(ARGV["tolerance_time_limit"]) then
+  redis.call("ZREMRANGEBYRANK", KEYS["user_lock"], 0, tonumber(ARGV["max_locks"])*-1-1)
 end
 
 local created_at = redis.call("ZSCORE", KEYS["user_lock"], ARGV["lock_id"])
 if not(created_at) or tonumber(created_at) < tonumber(ARGV["expired_time_limit"]) then
-	return "NOT_FOUND"
+  return "NOT_FOUND"
 else
-	redis.call("ZADD", KEYS["user_lock"], "XX", ARGV["now"], ARGV["lock_id"])
-	redis.call("PEXPIRE", KEYS["user_lock"], ARGV["ttl"])
-	return "OK"
+  redis.call("ZADD", KEYS["user_lock"], "XX", ARGV["now"], ARGV["lock_id"])
+  redis.call("PEXPIRE", KEYS["user_lock"], ARGV["ttl"])
+  return "OK"
 end
