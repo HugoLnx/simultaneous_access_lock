df510400cbede15288a790d50b088a4bcb8f8393
Hugo Roque <hugolnx@gmail.com>
2017-09-27 07:56:18 -0300

    Refactoring: Declaring args on lua scripts header

--- lib/lua/get_lock.lua
+++ lib/lua/get_lock.lua
@@ -1,27 +1,41 @@
-local locks_amount = redis.call("ZCOUNT", KEYS["user_lock"], ARGV["expired_time_limit"], math.huge)
+-- KEYS
+local key_user_lock = KEYS["user_lock"]
+local key_user_tolerance_start = KEYS["user_tolerance_start"]
 
-if locks_amount <= tonumber(ARGV["max_locks"]) then
-  redis.call("DEL", KEYS["user_tolerance_start"])
+-- ARGV
+local max_locks = tonumber(ARGV["max_locks"])
+local new_lock_id = ARGV["new_lock_id"]
+local now = ARGV["now"]
+local ttl = ARGV["ttl"]
+local expired_time_limit = tonumber(ARGV["expired_time_limit"])
+local tolerance_time_limit = tonumber(ARGV["tolerance_time_limit"])
+local tolerance_max_slots = tonumber(ARGV["tolerance_max_slots"])
+
+-- SCRIPT
+local locks_amount = redis.call("ZCOUNT", key_user_lock, expired_time_limit, math.huge)
+
+if locks_amount <= max_locks then
+  redis.call("DEL", key_user_tolerance_start)
 end
 
-if locks_amount < tonumber(ARGV["max_locks"]) then
-  redis.call("ZADD", KEYS["user_lock"], "NX", ARGV["now"], ARGV["new_lock_id"])
-  redis.call("PEXPIRE", KEYS["user_lock"], ARGV["ttl"])
+if locks_amount < max_locks then
+  redis.call("ZADD", key_user_lock, "NX", now, new_lock_id)
+  redis.call("PEXPIRE", key_user_lock, ttl)
   return "OK"
 else
-  if locks_amount >= tonumber(ARGV["tolerance_max_slots"]) then
+  if locks_amount >= tolerance_max_slots then
     return "NO_SLOTS"
   else
-    local tolerance_start_time = redis.call("GET", KEYS["user_tolerance_start"])
+    local tolerance_start_time = redis.call("GET", key_user_tolerance_start)
     if tolerance_start_time
-      and tonumber(tolerance_start_time) < tonumber(ARGV["tolerance_time_limit"])
-      and locks_amount > tonumber(ARGV["max_locks"]) then
+      and tonumber(tolerance_start_time) < tolerance_time_limit
+      and locks_amount > max_locks then
       return "NO_SLOTS"
     else
-      redis.call("ZADD", KEYS["user_lock"], "NX", ARGV["now"], ARGV["new_lock_id"])
-      redis.call("PEXPIRE", KEYS["user_lock"], ARGV["ttl"])
-      if not(tolerance_start_time) and locks_amount >= tonumber(ARGV["max_locks"]) then
-        redis.call("SET", KEYS["user_tolerance_start"], ARGV["now"])
+      redis.call("ZADD", key_user_lock, "NX", now, new_lock_id)
+      redis.call("PEXPIRE", key_user_lock, ttl)
+      if not(tolerance_start_time) and locks_amount >= max_locks then
+        redis.call("SET", key_user_tolerance_start, now)
       end
       return "OK"
     end
--- lib/lua/renew_lock.lua
+++ lib/lua/renew_lock.lua
@@ -1,14 +1,27 @@
-local tolerance_start_time = redis.call("GET", KEYS["user_tolerance_start"])
+-- KEYS
+local key_user_lock = KEYS["user_lock"]
+local key_user_tolerance_start = KEYS["user_tolerance_start"]
+
+-- ARGV
+local max_locks = tonumber(ARGV["max_locks"])
+local lock_id = ARGV["lock_id"]
+local now = ARGV["now"]
+local ttl = ARGV["ttl"]
+local expired_time_limit = tonumber(ARGV["expired_time_limit"])
+local tolerance_time_limit = tonumber(ARGV["tolerance_time_limit"])
+
+-- SCRIPT
+local tolerance_start_time = redis.call("GET", key_user_tolerance_start)
 if tolerance_start_time
-  and tonumber(tolerance_start_time) < tonumber(ARGV["tolerance_time_limit"]) then
-  redis.call("ZREMRANGEBYRANK", KEYS["user_lock"], 0, tonumber(ARGV["max_locks"])*-1-1)
+  and tonumber(tolerance_start_time) < tolerance_time_limit then
+  redis.call("ZREMRANGEBYRANK", key_user_lock, 0, max_locks*-1-1)
 end
 
-local created_at = redis.call("ZSCORE", KEYS["user_lock"], ARGV["lock_id"])
-if not(created_at) or tonumber(created_at) < tonumber(ARGV["expired_time_limit"]) then
+local created_at = redis.call("ZSCORE", key_user_lock, lock_id)
+if not(created_at) or tonumber(created_at) < expired_time_limit then
   return "NOT_FOUND"
 else
-  redis.call("ZADD", KEYS["user_lock"], "XX", ARGV["now"], ARGV["lock_id"])
-  redis.call("PEXPIRE", KEYS["user_lock"], ARGV["ttl"])
+  redis.call("ZADD", key_user_lock, "XX", now, lock_id)
+  redis.call("PEXPIRE", key_user_lock, ttl)
   return "OK"
 end
